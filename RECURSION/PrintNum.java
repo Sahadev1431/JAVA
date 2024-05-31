b66221f6b7a6f81b464b458f7948ff90ff70b91c
Sahadev1431 <naisahadev@gmail.com>
2024-05-31 08:54:33 +0530

    Recursion

---
+++ RECURSION/PrintNum.java
@@ -0,0 +1,15 @@
+package RECURSION;
+
+public class PrintNum {
+    public static void main(String[] args) {
+        r1(1);
+    }
+
+    static void r1(int n) {
+        if (n>5) {
+            return;
+        }
+        System.err.println(n);
+        r1(n+1);
+    }
+}
